08bd6a261489579afbe3486a93187a12823c95ad
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-06-01 16:43:55 +0200

    add dotenv

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,70 @@
 import React, { useState } from 'react'
 import './Home.css'
 import { TextField } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import axios from 'axios'
+require('dotenv').config()
 
 
 
 
-export default function Home() {
+
+
+export default function Home(props) {
+    // props à passer weather[0].main main.temp name pour l'afficher sur la 2eme page
+    
     const [query, setQuery] = useState('')
-    const [weather, setWeather] = useState({})
+    const [currentWeather, setCurrentWeather] = useState({})
     
+    const [showData, setShowData] = useState(null)
+    //const ApiKey = process.env
+    //const ApiUrl = process.env
     const ApiKey = '98b7465353d383f3d0f3bc4a284a48ae'
-    const ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${ApiKey}`
+    const ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${ApiKey}`
     
-    const getWeather = (event) => {
-            if (event.key === "Enter") {
-                 axios.get(ApiUrl)
-                      
-                      .then(result => {
-                       setWeather(result);
-                       setQuery('');
-                       console.log(result);
-                })
-            }
+    const getWeather = async() =>  {
+       await axios.get(ApiUrl)
+            .then(res => res.data)
+            .then(data => {
+            setQuery('');
+            setCurrentWeather(data)
+            setShowData(
+            <div>
+                <p>{data.name}</p>
+                <p>{Math.round(data.main.temp)}°C</p>
+                <p>{data.weather[0].main}</p>
+            </div>)
+            })
+            
     }
     return (
-        <>
-        <div className='container'>
-            <h3>What's the weather in your town ??</h3>
-                <div className="input">
-                    <TextField
-                        id="outlined-basic"
-                        label="city"
-                        variant="outlined" 
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                        onKeyPress={getWeather} 
+    <>
+      <div className='container'>
+        <h3>What's the weather in your town ??</h3>
+            <div className="input">
+                <TextField
+                    id="outlined-basic"
+                    label="city"
+                    variant="outlined" 
+                    value={query}
+                    required={true}
+                    onChange={(e) => setQuery(e.target.value)} 
                     />
-                </div>
-                <h3>{weather.name}</h3>
-
-
+                    <Button
+                    className="button"
+                    type="button"
+                    variant="outlined" 
+                    onClick={getWeather}>Submit
+                    </Button>
+            </div>
+                <div className="weather">
+                {showData}
+                <p>{/*data.weather[0].main*/}</p>
+            </div>
         </div>
-        
-            
-        </>
+       
+  
+           
+    </>
     )
 }
